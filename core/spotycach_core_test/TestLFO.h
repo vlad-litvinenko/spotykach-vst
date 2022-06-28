37bfbc75b0ad05f26ec9c7014ba7e41ac808e21a
Vlad <vla2020@icloud.com>
2022-06-28 18:26:10 +0200

    Add test LFO

---
+++ core/spotycach_core_test/TestLFO.h
@@ -0,0 +1,23 @@
+//
+//  TestLFO.h
+//  spotykach_core
+//
+//  Created by Vladyslav Lytvynenko on 28.06.22.
+//
+
+#ifndef TestLFO_h
+#define TestLFO_h
+
+#include <gmock/gmock.h>
+#include "ILFO.h"
+
+class TestLFO: public ILFO {
+public:
+    MOCK_METHOD(void, setAmplitude, (double), (override));
+    MOCK_METHOD(void, setPeriod, (double), (override));
+    MOCK_METHOD(void, setCurrentBeat, (double), (override));
+    MOCK_METHOD(void, setFramesPerMeasure, (long), (override));
+    MOCK_METHOD(float, triangleValueAt, (int), (override));
+};
+
+#endif /* TestLFO_h */
